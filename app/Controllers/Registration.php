7fd428c141aebbb000b3d8f6ed7ca2576c2049af
Mohamed Qabbani <mohamed.qabbani@gmail.com>
2019-03-13 20:05:14 +0000

    add unit test and refactor from name and email

--- app/Controllers/Registration.php
+++ app/Controllers/Registration.php
@@ -13,4 +13,19 @@ class Registration
     {
         require_once 'app/Views/registration.php';
     }
+
+    // Validation methods
+    public function validName($name)
+    {
+        $pattern = '/^[a-zA-Z]{2,20}$/i';
+
+        return preg_match($pattern, $name);
+    }
+
+    public function validEmail($email)
+    {
+        $pattern = '/^((?:[A-Za-z0-9_.-][A-Za-z0-9_.-]+))(@)([a-z]+)(\\.)(com)$/';
+
+        return preg_match($pattern, $email);
+    }
 }
--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -4,8 +4,38 @@ use PHPUnit\Framework\TestCase;
 
 class UnitTest extends TestCase
 {
-    public function testPushAndPop()
+    public function testName()
     {
+        $name1 = 'aBcAbC'; // valid
+        $name2 = 'abc4A'; // not valid
+        $name3 = 'a'; // not valid
+        $name4 = 'ddddddddddddddddddddddddddddddddddddddddddddfffffffffffffffffffffff'; // not valid
+
+        $reg = new \App\Controllers\Registration();
+        $this->assertEquals(1, $reg->validName($name1));
+        $this->assertEquals(0, $reg->validName($name2));
+        $this->assertEquals(0, $reg->validName($name3));
+        $this->assertEquals(0, $reg->validName($name4));
+
+    }
+
+    public function testEmail()
+    {
+        $name1 = 'foo@example.com'; // valid
+        $name2 = 'Foo.noo@example.com'; // valid
+        $name3 = 'foo_noo@example.com'; // valid
+        $name4 = 'Foo@Example.com'; // not valid
+        $name5 = 'foo~noo@example.com'; // not valid
+        $name6 = 'foo@example.net'; // not valid
+
+        $reg = new \App\Controllers\Registration();
+
+        $this->assertEquals(1, $reg->validEmail($name1));
+        $this->assertEquals(1, $reg->validEmail($name2));
+        $this->assertEquals(1, $reg->validEmail($name3));
+        $this->assertEquals(0, $reg->validEmail($name4));
+        $this->assertEquals(0, $reg->validEmail($name5));
+        $this->assertEquals(0, $reg->validEmail($name6));
 
     }
 }
